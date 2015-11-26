8697b0139e4abfea1550f655f99d9b6ea5bea4f1
tyler <josephtylerjones@gmail.com>
2015-11-25 20:43:50 -0800

    updated install

--- install.sh
+++ install.sh
@@ -3,8 +3,27 @@
 
 cd ~
 
-if [ -d home ]; then
-    echo "home exists"
+# Detect platform
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    os=osx
+elif [[ "$OSTYPE" == "linux"* ]]; then
+    os=linux
 else
-    echo "home doesn't exist"
+    echo "Could not detect operating system."
+    exit 1
 fi
+
+# Make sure we have the "home" repo
+if [[ -d home ]]; then
+    cd home
+    git pull origin master
+else
+    git clone git@github.com:josephtyler/home.git home
+fi
+
+# Make the sym links
+ln -s ~/home/bin ~/bin
+ln -s ~/home/src ~/src
+ln -s ~/home/.vim ~/.vim
+ln -s ~/home/.vimrc ~/.vimrc
+ln -s ~/home/.zshrc ~/.zshrc
--- make_links.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -s ~/home/bin ~/bin
-ln -s ~/home/src ~/src
-ln -s ~/home/.vim ~/.vim
-ln -s ~/home/.vimrc ~/.vimrc
